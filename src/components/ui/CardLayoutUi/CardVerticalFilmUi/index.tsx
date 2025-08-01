89309cf9a9491fa383910b8b46fe799d6b60ef54
HaiToan <toan53011@gmail.com>
2025-08-01 18:17:13 +0700

    update: collection list , ranking

--- src/components/ui/CardLayoutUi/CardVerticalFilmUi/index.tsx
+++ src/components/ui/CardLayoutUi/CardVerticalFilmUi/index.tsx
@@ -21,9 +21,9 @@ const CardVerticalFilmUi = (props: Props) => {
       <div className="aspect-9/16 h-[316px] w-full rounded-lg overflow-hidden relative cursor-pointer hover:opacity-[0.8] opacity-100" key={props._id}>
         <Image fill src={convertStaticImage(props.images.posters[0].path)} className="w-full h-full object-cover" alt="" />
         <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex font-thin text-xs">
-          {props.latest_episode?.["1"] && <div className={`bg-[#5e6070] py-0.5 px-2 ${!props.latest_episode?.["4"] && !props.latest_episode?.["2"] ? "rounded-t-sm" : "rounded-tl-sm"}`}>PĐ. {props.latest_episode?.["1"]}</div>}
-          {props.latest_episode?.["2"] && <div className={`bg-[#1667cf] py-0.5 px-2 ${!props.latest_episode?.["4"] && !props.latest_episode?.["1"] ? "rounded-t-sm" : props.latest_episode?.["4"] && !props.latest_episode?.["1"] ? "rounded-tl-sm" : props.latest_episode?.["1"] && !props.latest_episode?.["4"] ? "rounded-tr-sm" : ""}`}>LT. {props.latest_episode?.["2"]}</div>}
-          {props.latest_episode?.["4"] && <div className={`bg-[#2ca35d] py-0.5 px-2 ${!props.latest_episode?.["1"] && !props.latest_episode?.["2"] ? "rounded-t-sm" : "rounded-tr-sm"}`}>TM. {props.latest_episode?.["4"]}</div>}
+          {props.latest_episode?.["1"] && <div className={`bg-[#5e6070] py-0.5 px-2 ${!props.latest_episode?.["4"] && !props.latest_episode?.["2"] ? "rounded-t-sm" : "rounded-tl-sm"}`}>P.Đề</div>}
+          {props.latest_episode?.["2"] && <div className={`bg-[#1667cf] py-0.5 px-2 ${!props.latest_episode?.["4"] && !props.latest_episode?.["1"] ? "rounded-t-sm" : props.latest_episode?.["4"] && !props.latest_episode?.["1"] ? "rounded-tl-sm" : props.latest_episode?.["1"] && !props.latest_episode?.["4"] ? "rounded-tr-sm" : ""}`}>L.Tiếng</div>}
+          {props.latest_episode?.["4"] && <div className={`bg-[#2ca35d] py-0.5 px-2 ${!props.latest_episode?.["1"] && !props.latest_episode?.["2"] ? "rounded-t-sm" : "rounded-tr-sm"}`}>T.Minh</div>}
         </div>
       </div>
       <div className="mt-3 text-center">
